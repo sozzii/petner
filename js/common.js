4430320ecd1a284f9e2c12c1dca29bfb63a9f08a
dbwjd6006 <dldbwjd626@gmai.com>
2022-07-29 12:18:28 +0900

    add10

--- js/common.js
+++ js/common.js
@@ -51,10 +51,15 @@ function init(){
 
     //sub2_accordion menu
     $(".sub2-1 .sub2-1-menu1").click(function() {
-        $(this).next(".sub2-1 .sub2-1-menu2").stop().slideToggle(400); // : 1개
-        $(this).toggleClass('on').siblings().removeClass('on');
-        $(this).next(".sub2-1 .sub2-1-menu2").siblings(".sub2-1 .sub2-1-menu2").slideUp(); // 1개는 닫히고, 1개는 펼쳐짐
+        $(".sub2-1 .sub2-1-menu2").slideUp();
+        $(this).next().stop().slideToggle(400);
+        $(this).toggleClass('on');
     });
+    // $(".sub2-1 .sub2-1-menu1").click(function() {
+    //     $(this).next(".sub2-1 .sub2-1-menu2").stop().slideToggle(400); // : 1개
+    //     $(this).toggleClass('on').siblings().removeClass('on');
+    //     $(this).next(".sub2-1 .sub2-1-menu2").siblings(".sub2-1 .sub2-1-menu2").slideUp(); // 1개는 닫히고, 1개는 펼쳐짐
+    // });
 
     $(".sub2-2-list").click(function() {
         alert('페이지 준비중입니다~!');
@@ -70,19 +75,25 @@ function init(){
         }
     });
 
-    $(".button01").click(function(){
-        $(".button02").addClass('remove');
-        $(".button01").addClass('active');
-        $(".button02 p").addClass('remove');
-        $(".button01 p").addClass('active');
-    })
-    $(".button02").click(function(){
-        $(".button01").addClass('remove');
-        $(".button02").addClass('active');
-        $(".button01 p").addClass('remove');
-        $(".button02 p").addClass('active');
-    })
+    // $(".button01").click(function(){
+    //     $(".button02").addClass('remove');
+    //     $(".button01").addClass('active');
+    //     $(".button02 p").addClass('remove');
+    //     $(".button01 p").addClass('active');
+    // })
+    // $(".button02").click(function(){
+    //     $(".button01").addClass('remove');
+    //     $(".button02").addClass('active');
+    //     $(".button01 p").addClass('remove');
+    //     $(".button02 p").addClass('active');
+    // })
 
+    //sub4-2 accordial menu
+    $(".board ul li p").click(function() {
+        $(".board ul li div").slideUp();
+        $(this).next().stop().slideToggle(400);
+        $(this).toggleClass('on');
+    });
 
 }
 
